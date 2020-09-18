57db2f8f3365a4d33092c5424bf7b5f0ca2cbd8f
Nemistic <bosebcc@hotmail.com>
2020-09-18 18:33:52 +0700

    setup board - fix error management // fix boardRenderer()

--- elements.py
+++ elements.py
@@ -1,9 +1,10 @@
-#import
+# import
 alphabet = []
-for letter in range(97,123):
+for letter in range(97, 123):
     alphabet.append(chr(letter))
 
-#main functions
+# main functions
+
 
 def boardInput():
     w = 0
@@ -11,13 +12,13 @@ def boardInput():
     while True:
         try:
             w = int(input("Enter the width of the board: "))
+        except ValueError:
+            print("Invalid value, please try again")
+        else:
             if w > 26 or w <= 0:
                 print("Maximum width is 26, please try again")
             else:
                 break
-        except ValueError:
-            print("Invalid value, please try again")
-            break              
 
     while True:
         try:
@@ -27,33 +28,33 @@ def boardInput():
         else:
             break
 
-    return [w,h]
+    return [w, h]
 
 
+def boardGenerator(w, h):
 
-def boardGenerator(w,h):
-    
     board = []
     for a in range(w*h):
         board.append(0)
     return board
 
-def boardRenderer(board,w,h):
-    print("   ", end='') #Space
-    #Index Row using a-z
+
+def boardRenderer(board, w, h):
+    print("   ", end='')  # Space
+    # Index Row using a-z
     for a in range(w):
         print(" ", end='')
         print(alphabet[a], end='')
     print()
-    #Render The Board Out wih Column index
-    for a in range(w):
-        print(a+1, end='') #index
-        if a<9:
-            print("   ", end='') #spacing after index for 1-digit
+    # Render The Board Out wih Column index
+    for a in range(h):
+        print(a+1, end='')  # index
+        if a < 9:
+            print("   ", end='')  # spacing after index for 1-digit
         else:
-            print("  ", end='') #spacing after index for 2-digit
-        #board rendering
-        for b in range(h): 
+            print("  ", end='')  # spacing after index for 2-digit
+        # board rendering
+        for b in range(w):
             print(board[a], end='')
             print(" ", end='')
         print()
