e746b868af3aecc871d0d03fb24d476cf3418a77
Kan Soponkij <kansoponkij@Kans-MacBook-Pro-2.local>
2020-09-18 15:29:04 +0700

    setup board - add user board size customization

--- elements.py
+++ elements.py
@@ -1,15 +1,59 @@
 #import
+alphabet = []
+for letter in range(97,123):
+    alphabet.append(chr(letter))
 
 #main functions
+
+def boardInput():
+    w = 0
+    h = 0
+    while True:
+        try:
+            w = int(input("Enter the width of the board: "))
+            if w > 26 or w <= 0:
+                print("Maximum width is 26, please try again")
+            else:
+                break
+        except ValueError:
+            print("Invalid value, please try again")
+            break              
+
+    while True:
+        try:
+            h = int(input("Enter the height of the board: "))
+        except ValueError:
+            print("Invalid value, please try again")
+        else:
+            break
+
+    return [w,h]
+
+
+
 def boardGenerator(w,h):
+    
     board = []
     for a in range(w*h):
         board.append(0)
     return board
 
 def boardRenderer(board,w,h):
+    print("   ", end='') #Space
+    #Index Row using a-z
     for a in range(w):
-        for b in range(h):
+        print(" ", end='')
+        print(alphabet[a], end='')
+    print()
+    #Render The Board Out wih Column index
+    for a in range(w):
+        print(a+1, end='') #index
+        if a<9:
+            print("   ", end='') #spacing after index for 1-digit
+        else:
+            print("  ", end='') #spacing after index for 2-digit
+        #board rendering
+        for b in range(h): 
             print(board[a], end='')
             print(" ", end='')
         print()
--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 from elements import *
 
-w = 10
-h = 10
+#welcome menu
+print("----------------------------------------------")
+print("\n\n\n\n     ///   BattleShip by Kan & Bose   ///     \n\n\n\n")
+print("----------------------------------------------")
+try:
+    input("\nPress enter to continue...")
+except SyntaxError:
+    pass
 
-board = boardGenerator(w,h)
-boardRenderer(board, w,h)
+
+
+
+
+#generate board and pass it to var:board
+
+boardSize = boardInput()
+
+board = boardGenerator(boardSize[0],boardSize[1]) 
+
+#Render board by passing var:board to render function
+boardRenderer(board,boardSize[0],boardSize[1])
+
+#menu
+#print("///   Welcome to BattleShip   ///")
+#print("\n\n\n")
